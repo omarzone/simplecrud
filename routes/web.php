31fe41b148728f1be080997c148e7b67f82b9aa4
Omar <omar231120@hotmail.com>
2022-12-07 21:23:07 -0600

    Vistas,controladores, routes y migracion creada

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AlumnoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Llmar a una vista apuntando a su blade.php
+// Route::get('/alumno', function () {
+//     return view('alumno.index');
+// });
+
+//Llamar a una vista apuntando a una funcion del controlador
+// Route::get('alumno/create',[AlumnoController::class,'create']);
+
+
+//Genera todas las urls en funcion de las funciones del controlador
+Route::resource('alumno',AlumnoController::class);
+
